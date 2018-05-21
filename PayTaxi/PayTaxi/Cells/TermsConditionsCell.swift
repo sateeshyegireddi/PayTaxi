64c09b4c0da05c61de6d6e744658e817dab58d48
Sateesh <yegireddi.sateesh@gmail.com>
2018-05-21 21:14:32 +0530

    --- Driver --- - Create Terms & Conditions title in Registration Screen - Saving user data in models in Registration Screen - Fix UI issues - Add new images

---
+++ PayTaxi/PayTaxi/Cells/TermsConditionsCell.swift
@@ -0,0 +1,24 @@
+//
+//  TermsConditionsCell.swift
+//  PayTaxi
+//
+//  Created by Sateesh Yegireddi on 21/05/18.
+//  Copyright © 2018 PayTaxi. All rights reserved.
+//
+
+import UIKit
+
+class TermsConditionsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
